728f63eb6a5c7f188d60450c6b9491ebb3e13e8a
prabs <prabs@prabs.(none)>
2012-08-31 11:30:10 +0530

    for loop equivalent

---
+++ chapter2/2-4.c
@@ -0,0 +1,12 @@
+void squeeze(char s1[], char s2[])
+{
+	
+	int i, j ,k;
+	
+	for (i = j = 0; s1[i] != '\0'; i++) {
+		 for (k = 0; s2[k] != '\0'; k++)
+			if (s2[k] != s1[i])
+				s1[j++] = s1[i];
+	}
+	s1[j] = '\0';
+}
